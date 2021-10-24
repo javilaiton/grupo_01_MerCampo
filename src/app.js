6b10e426eb3a78c953371bc0d003c63a649d6a71
Taniacardenas <tania.cardenas103@gmail.com>
2021-10-23 20:47:19 -0500

    add estructure basic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
-/* ACÁ SOLO SE CONFIGURA EL SERVIDOR */
+const express = require ('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+}) 
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/product_details.html'))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/shopping_cart.html'))
+})
+
+
+app.listen(port, () => console.log(`Server in port ${port}`));
+
+
+
+
+
