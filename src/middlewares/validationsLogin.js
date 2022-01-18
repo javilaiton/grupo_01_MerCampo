6f63f9367f4735202b62b179382824f5122d51c9
Ferain93 <fer.tn7@gmail.com>
2022-01-18 16:17:20 -0500

    add: validationsLogin middlewares

---
+++ src/middlewares/validationsLogin.js
@@ -0,0 +1,8 @@
+const { body } = require('express-validator');
+
+const validationsLogin = [
+    body('email').notEmpty().withMessage('Escribe tu correo')
+    .isEmail().withMessage('Ingresa un correo válido')
+];
+
+module.exports =  validationsLogin;
