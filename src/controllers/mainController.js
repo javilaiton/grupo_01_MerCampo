134f4f88e386dec943a7b71e23f15b49ec146869
Ferain93 <fer.tn7@gmail.com>
2022-01-18 16:07:45 -0500

    add: adjust mainController with db

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,16 @@
+const db = require('../database/models');
+const productsModel = require('../model/productsModel');
 const controller = {
-    home: (req,res)=>{
-        res.render("home")
+    home: async(req,res)=>{
+        try{
+            let productos = await productsModel.getList()
+            res.render("home", {productos})
+
+        }catch(error){
+            console.log(error)
+
+        }
+        
     },
     product_details:(req,res)=>{
         res.render("products/product_details")
